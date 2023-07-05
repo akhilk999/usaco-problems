b4c2a02869ac09426d084e84fb3d6064b53d4fe6
Akhil Kasamsetty <awesomesetty99@gmail.com>
2023-07-05 18:46:19 -0500

    USACO 2015 December Contest, Bronze Problem 1. Fence Painting

---
+++ src/FencePainting/FencePainting.java
@@ -0,0 +1,24 @@
+//USACO 2015 December Contest, Bronze
+//Problem 1. Fence Painting
+
+package FencePainting;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.File;
+public class FencePainting {
+    public static void main(String[] args) throws IOException{
+        Scanner scan = new Scanner(new File("paint.in"));
+        PrintWriter print = new PrintWriter("paint.out"));
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+
+
+    }
+
+}
